329f9d24f554d893c0cbea21c8b3537ab3513532
Robert <robertmonaco@ou.edu>
2018-09-23 11:38:04 -0500

    Added qlearn.py, file structure

---
+++ learning/qlearn.py
@@ -0,0 +1,6 @@
+#GaRLaga
+#Robert Monaco
+#CS 4033 - University of Oklahoma - Dr. Amy McGovern
+#Long Reinforcement Learning Project
+#Creates an instance simulating an agent learning Galaga with Q-learning
+
