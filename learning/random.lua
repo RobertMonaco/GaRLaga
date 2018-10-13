bf466f7f353f91bb826219f1d7330ab172fd9f44
Robert <robertmonaco@ou.edu>
2018-10-13 12:10:36 -0500

    Added qlearning script, modified script to end after 1 life or 5000 points. Added file system for outputs.

--- learning/random.lua
+++ learning/random.lua
@@ -1,14 +1,15 @@
 --emu:loadrom('galaga.zip')
+--require("numlua")
 
 inputTable = joypad.read(1);
 framecount = 1
-for i= 0,5,1 do
+for i= 0,10000,1 do
   savestate.load(savestate.object(10));
-  is_not_dead = true;
-  local file = io.open("outputs/outputTest_" .. i .. ".csv", "w");
+  continue_sim = true;
+  local file = io.open("outputs/random/outputTest_" .. i .. ".csv", "w");
   step_action = 0;
   
-  while is_not_dead do
+  while continue_sim do
     joypad.set(1, inputTable)
     if(framecount % 20 == 0) then
       framecount = 1
@@ -26,8 +27,8 @@ for i= 0,5,1 do
           inputTable['right'] = true;
         end;
       end;
-      print(input_key)
-      print(inputTable)
+      --print(input_key)
+      --print(inputTable)
     end;
 
     --position is 8 - 183
@@ -40,16 +41,16 @@ for i= 0,5,1 do
     score5 = memory.readbyte(229);
     score6 = memory.readbyte(230);
     lives = memory.readbyte(1159);
-    print(memory.readbyte(340))
-    print(memory.readbyte(341))
-    print(memory.readbyte(342))
-    print(memory.readbyte(343))
-    print(memory.readbyte(344))
-    print(memory.readbyte(345))
-    print(memory.readbyte(346))
-    print(memory.readbyte(347))
-    print(memory.readbyte(348))
-    print(memory.readbyte(349))
+    --print(memory.readbyte(340))
+    --print(memory.readbyte(341))
+    --print(memory.readbyte(342))
+    --print(memory.readbyte(343))
+    --print(memory.readbyte(344))
+    --print(memory.readbyte(345))
+    --print(memory.readbyte(346))
+    --print(memory.readbyte(347))
+    --print(memory.readbyte(348))
+    --print(memory.readbyte(349))
   
     score = score0 * 10^6 + score1 * 10^5 + score2 * 10^4 + score3 * 10^3 + score4 * 10^2 + score5 * 10 + score6;
     
@@ -66,8 +67,8 @@ for i= 0,5,1 do
     step_action = step_action + 1
     emu.frameadvance();
 
-    if lives == 0 and memXPos == 0 then
-      is_not_dead = false;
+    if lives < 2 or score > 5000 then
+      continue_sim = false;
     end;
   end;
 
