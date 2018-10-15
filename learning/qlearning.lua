0e5cdb693aa8eaee66c86f1a1320e27a22566659
Robert <robertmonaco@ou.edu>
2018-10-14 19:44:14 -0500

    Made simple qlearning with bullets and xpos work

--- learning/qlearning.lua
+++ learning/qlearning.lua
@@ -17,23 +17,23 @@ keys = {}
 --generate all possible strings
 for pos=8,183,1 do
   pos_str = append_zeros_to_left(pos,3)
-  for enemy=0,88888,1 do
-    enemy_str = append_zeros_to_left(enemy,5)
-    if string.find(enemy_str,"9") == nil then
+  --for enemy=0,88888,1 do
+    --nemy_str = append_zeros_to_left(enemy,5)
+    --if string.find(enemy_str,"9") == nil then
       for bullet1=0,1,1 do
         for bullet2=0,1,1 do
-          final_str= "" .. pos_str .. enemy_str .. bullet1 .. bullet2
+          final_str= "" .. pos_str .. bullet1 .. bullet2--enemy_str .. bullet1 .. bullet2
           table.insert(keys, final_str)
           print(final_str)
         end;
       end;
-    end;
-  end;
+    --end;
+  --end;
 end;
 
 --for each key, add key to table, initialize all table actions to zero
 for k=1,#keys,1 do
-  q_table[keys[k]] = {0,0,0,0,0}
+  q_table[keys[k]] = {0,0,0,0,0,0}
 end;
 
 print("Table initialized.")
@@ -52,7 +52,7 @@ end;
 
 function choose_action(current_state,q_table,epsilon)
   if math.random() < epsilon then
-    return math.random(0,5);
+    return math.random(1,6);
   end;
   
   --choose action that grants max q given current_state
@@ -79,23 +79,26 @@ function get_reward(score, past_score)
   elseif past_score - score > 0 then
     return 0;
   end;
+  return -1;
 end;
 
 --get state from memory bytes
 function get_state()
-  pos = memory.readbyte(515)
-  erow_1 = memory.readbyte(1024) + memory.readbyte(1025) + memory.readbyte(1026) + memory.readbyte(1027) + memory.readbyte(1028) + memory.readbyte(1029) + memory.readbyte(1030) + memory.readbyte(1031) + memory.readbyte(1032)
-  erow_2 = memory.readbyte(1040) + memory.readbyte(1041) + memory.readbyte(1042) + memory.readbyte(1043) + memory.readbyte(1044) + memory.readbyte(1045) + memory.readbyte(1046) + memory.readbyte(1047) + memory.readbyte(1048)
-  erow_3 = memory.readbyte(1056) + memory.readbyte(1057) + memory.readbyte(1058) + memory.readbyte(1059) + memory.readbyte(1060) + memory.readbyte(1061) + memory.readbyte(1062) + memory.readbyte(1063) + memory.readbyte(1064)
-  erow_4 = memory.readbyte(1072) + memory.readbyte(1073) + memory.readbyte(1074) + memory.readbyte(1075) + memory.readbyte(1076) + memory.readbyte(1077) + memory.readbyte(1078) + memory.readbyte(1079) + memory.readbyte(1080)
-  erow_5 = memory.readbyte(1088) + memory.readbyte(1089) + memory.readbyte(1090) + memory.readbyte(1091) + memory.readbyte(1092) + memory.readbyte(1093) + memory.readbyte(1094) + memory.readbyte(1095) + memory.readbyte(1096)
-  b1 = memory.readbyte(736) / 128
-  b2 = memory.readbyte(744) / 128
-  return "" .. pos .. erow_1 .. erow_2 .. erow_3 .. erow_4 .. erow_5 .. b1 .. b2;
+  pos = append_zeros_to_left(memory.readbyte(515),3)
+
+  --erow_1 = memory.readbyte(1024) + memory.readbyte(1025) + memory.readbyte(1026) + memory.readbyte(1027) + memory.readbyte(1028) + memory.readbyte(1029) + memory.readbyte(1030) + memory.readbyte(1031) + memory.readbyte(1032)
+  --erow_2 = memory.readbyte(1040) + memory.readbyte(1041) + memory.readbyte(1042) + memory.readbyte(1043) + memory.readbyte(1044) + memory.readbyte(1045) + memory.readbyte(1046) + memory.readbyte(1047) + memory.readbyte(1048)
+  --erow_3 = memory.readbyte(1056) + memory.readbyte(1057) + memory.readbyte(1058) + memory.readbyte(1059) + memory.readbyte(1060) + memory.readbyte(1061) + memory.readbyte(1062) + memory.readbyte(1063) + memory.readbyte(1064)
+  --erow_4 = memory.readbyte(1072) + memory.readbyte(1073) + memory.readbyte(1074) + memory.readbyte(1075) + memory.readbyte(1076) + memory.readbyte(1077) + memory.readbyte(1078) + memory.readbyte(1079) + memory.readbyte(1080)
+  --erow_5 = memory.readbyte(1088) + memory.readbyte(1089) + memory.readbyte(1090) + memory.readbyte(1091) + memory.readbyte(1092) + memory.readbyte(1093) + memory.readbyte(1094) + memory.readbyte(1095) + memory.readbyte(1096)
+  b1 = math.floor(memory.readbyte(736) / 128)
+  b2 = math.floor(memory.readbyte(744) / 128)
+  print("" .. pos .. ", " .. b1 .. ", " .. b2)
+  return "" .. pos .. b1 .. b2--erow_1 .. erow_2 .. erow_3 .. erow_4 .. erow_5 .. b1 .. b2;
 end;
 
 --update q_table based on current q values, reward, discount rate, learning rate
-function update_table(prev_state,curr_state,a, r,dr, lr)
+function update_table(q_table,prev_state,curr_state,a, r,dr, lr)
   --find maximum q value based on new state
   max_val = math.max(q_table[curr_state][1],q_table[curr_state][2],q_table[curr_state][3],q_table[curr_state][4],q_table[curr_state][5],q_table[curr_state][6])
   q_table[prev_state][a] = q_table[prev_state][a] + lr * (r + dr * max_val - q_table[prev_state][a])
@@ -114,14 +117,17 @@ for i= 0,10000,1 do
   score = 0;
   past_score = 0;
   lives = memory.readbyte(1159);
-  state = "0080000000"
-  prev_state = "0080000000"
+  state = "00800"
+  prev_state = "00800"
+  reward = -1
+  action_val = 1
   alpha = 0.1
   gamma = 0.9
   epsilon = 1
   
   --Run Simulation until first death
   while lives >= 2 do
+    memXPos = memory.readbyte(515);
     --set joypad presses
     joypad.set(1, inputTable)
 
@@ -138,6 +144,7 @@ for i= 0,10000,1 do
       
       --update score
       past_score = score;
+      
       score0 = memory.readbyte(224);
       score1 = memory.readbyte(225);
       score2 = memory.readbyte(226);
@@ -155,7 +162,7 @@ for i= 0,10000,1 do
       state = get_state()
 
       --update q_table
-      update_table(prev_state,state,action_val,reward,gamma, alpha)
+      update_table(q_table,prev_state,state,action_val,reward,gamma, alpha)
 
       --clear joypad
       for k,v in pairs(inputTable) do
